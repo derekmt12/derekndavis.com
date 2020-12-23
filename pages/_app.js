e7ca7b5f4f0e1ee55b04fa66c3e60c89e482142c
Derek Davis <derekdavis@dereks-mbp.lan>
2020-12-23 07:19:19 -0600

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
+
 import 'prism-material-themes/themes/material-palenight.css';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }

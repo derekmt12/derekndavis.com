46d8ef8ed27817054dd1bfa1de56c1330307f1ab
Derek Davis <derekmt12@gmail.com>
2021-03-12 13:39:10 -0600

    splitbee analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,15 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import * as gtag from '../lib/gtag';
+import splitbee from '@splitbee/web';
 
 import 'prism-material-themes/themes/material-palenight.css';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
+  // initialize analytics
   useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+    splitbee.init();
+  }, []);
 
   return <Component {...pageProps} />;
 }
